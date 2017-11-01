72a1b53daa20e8225bc98abf7e8016667e83b073
LasseSode <lasse_sode@hotmail.com>
2017-11-01 12:18:05 +0100

    Dronning tingen virker.

--- Solver.java
+++ Solver.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.lang.*;
 
 /**
  * Write a description of class Solver here.
@@ -12,6 +13,8 @@ public class Solver
     public int noOfQueens;
     private int[] queens;
     private int noOfSolutions;
+    private boolean showSolutions; // styrer om løsningerne udskrives på terminalen
+    private long duration;
 
     /**
      * Constructor for objects of class Solver
@@ -21,27 +24,54 @@ public class Solver
         this.noOfQueens = noOfQueens;
         this.queens = new int[noOfQueens];
         this.noOfSolutions = noOfSolutions;
-    }
-
-    public void findAllSolutions(int noOfQuuens){
-
+        Arrays.fill(queens, 99);
+        this.duration = duration;
 
     }
 
-    public void testLegal(){
-        queens = new int[] {4,6,3,0,0,0,0,0};
-        System.out.println("Queens in: "+ Arrays.toString(queens)+" => Legal positions: ");
-        for(int i=0;i<8;i++){
-            if(legal(3,i)){
-                System.out.println(i+" ");
+   public void findAllSolutions(int noOfQueens){
+        /*System.out.format("   %3d  %,12d     %,8d     %,8d %n", noOfQueens, noOfSolutions,
+                duration, noOfSoultions/duration);*/
+       System.out.println("*****************************");
+       System.out.println("Solutions for " + noOfQueens + " queens:");
+
+       positionQueens(0);
+
+       System.out.println();
+       System.out.println();
+       duration = System.currentTimeMillis();
+       System.out.println("Were found in " + System.currentTimeMillis() + "s");
+       System.out.println("*****************************");
+
+    }
+
+    public void testLegal() {
+        queens = new int[]{4, 6, 3, 0, 0, 0, 0, 0};
+        System.out.println("Queens in: " + Arrays.toString(queens) + " => Legal positions: ");
+        for (int i = 0; i < 8; i++) {
+            if (legal(3, i)) {
+                System.out.println(i + " ");
             }
             System.out.println();
         }
+    }
 
 
-    //private void positionQueens(int row){
-
-
+    private void positionQueens(int row) {
+        for (int i = 0; i < noOfQueens; i++) {
+            if (legal(row, i)) {
+                queens[row] = i;
+                if (row + 1 < noOfQueens) {
+                    positionQueens(row + 1);
+                    queens[row] = 99;
+                } else {
+                    printSolution();
+                    queens[row] = 99;
+                }
+            } else {
+                queens[row] = 99;
+            }
+        }
     }
 
     private boolean legal(int row, int col) {
@@ -54,20 +84,31 @@ public class Solver
     }
 
     public static void main(String[] args) {
-        Solver s1 = new Solver(8);
-        s1.testLegal();
+        Solver s1 = new Solver(14);
+       // s1.findAllSolutions(14);
+        s1.findNoOfSolutions(7,9);
     }
 
+
+
+    private void printSolution() {
+
+       System.out.println();
+        for(int i = 0; i< queens.length; i++){
+                System.out.print(convert(i, queens[i]) + " ");
+
+        }
     }
 
-    //private void printSolution(){
+    public void findNoOfSolutions(int min, int max){
+
+       findAllSolutions(max-min);
+    }
+
+    private String convert(int row, int col) {
+        return String.valueOf((char)('a'+col))+(row+1);
+
+    }
 
 
-
-
-    //private String convert(int row, int col){
-
-
-
-
-
+}
